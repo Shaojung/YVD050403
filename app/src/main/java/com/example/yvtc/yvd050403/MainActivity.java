5017362134176122acc7fabb048ff07076aa1107
Shaojung Lu <shaojung@gmail.com>
2017-05-04 15:38:15 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yvtc/yvd050403/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.yvtc.yvd050403;
+
+import android.app.AlarmManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void click1(View v)
+    {
+        
+        AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
+        am.set(AlarmManager.RTC_WAKEUP, , );
+    }
+}
